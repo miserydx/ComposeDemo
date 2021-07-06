bf724cb02d47e0a90b3aa3284b0b5afa5aaafa9c
yangjiayi <yangjiayi@58.com>
2021-07-06 21:11:49 +0800

    添加状态管理示例

--- app/src/main/java/com/miserydx/compose58/StateActivity.kt
+++ app/src/main/java/com/miserydx/compose58/StateActivity.kt
@@ -1,11 +1,11 @@
 package com.miserydx.compose58
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -14,12 +14,28 @@ import androidx.compose.ui.unit.sp
 class StateActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        var s by mutableStateOf(1)
         setContent {
+//            var s = 1
+//            var s by mutableStateOf(1)
+//            var s by remember { mutableStateOf(1) }
+//            Text(
+//                text = s.toString(),
+//                modifier = Modifier.clickable {
+//                    s++
+//                }
+//            )
             Content()
         }
     }
 }
 
+/**
+ * Sample2
+ * 1.点击第一行字更新同作用域，第二行字会受到影响
+ * 2.如果不想第二行字受第一行刷新影响则把其抽成composable函数即可
+ * 3.此优化前提条件是composable函数不可内联，Column内联不可以，但Surface非内联可以
+ */
 @Composable
 fun Content() {
     var flag by remember { mutableStateOf(1) }
@@ -30,9 +46,7 @@ fun Content() {
             modifier = Modifier.clickable {
                 flag++
             }
-        ).also {
-            Log.e("misery", "第一行字")
-        }
+        )
         SecondText()
     }
 }
@@ -40,9 +54,17 @@ fun Content() {
 @Composable
 fun SecondText() {
     Text(
-        text = "第二行字",
+        text = (0..10).random().toString(),
         fontSize = 20.sp,
-    ).also {
-        Log.e("misery", "第二行字")
+    )
+}
+
+@Composable
+fun Counter() {
+    var count by remember { mutableStateOf(0) }
+    Button(
+        onClick = { count += 1 }
+    ) {
+        Text(text = "Count: $count")
     }
 }
